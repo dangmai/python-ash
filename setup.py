e66b37e9cbc23aa33e1d27540398a11444cd3e11
Dang Mai <contact@dangmai.net>
2013-01-22 16:49:27 -0500

    Prepare for distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Ash',
+    version='0.1.0',
+    author='Dang Mai',
+    author_email='contact@dangmai.net',
+    scripts=['bin/ash.py'],
+    url='https://github.com/dangmai/python-ash',
+    license='LICENSE.txt',
+    description='Thin wrapper for virtualenv for easy environment management',
+    long_description=open('README.rst').read()
+)
