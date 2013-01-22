96c4ca67c58633f37c7c481a16f729735310f7b5
Dang Mai <contact@dangmai.net>
2013-01-22 17:55:48 -0500

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Ash',
-    version='0.1.0',
+    version='0.1.1',
     author='Dang Mai',
     author_email='contact@dangmai.net',
     scripts=['bin/ash.py'],
